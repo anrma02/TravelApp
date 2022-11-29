71fca64e0594f880e0c094b5fb85205e1d848e4e
Nguyễn Xuân An <113039601+anrma02@users.noreply.github.com>
2022-11-29 17:17:07 +0700

    Login & Register

---
+++ assets/font/color.js
@@ -0,0 +1,5 @@
+export const COLOR = {
+  mainColor: "#1e2027",
+  secondaryColor: "#FB741D",
+  CtgColor: "#111317",
+};
--- src/login/login.js
+++ src/login/login.js
@@ -1,12 +1,241 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import {
+  Dimensions,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+} from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  interpolate,
+  withTiming,
+  withDelay,
+  runOnJS,
+  withSequence,
+  withSpring,
+} from "react-native-reanimated";
+import Svg, { Ellipse, ClipPath } from "react-native-svg";
 
+const { height, width } = Dimensions.get("window");
 export default function login() {
+  const imagePosition = useSharedValue(1);
+  const formButtonScale = useSharedValue(1);
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const imageAnimatedStyle = useAnimatedStyle(() => {
+    const interpolation = interpolate(
+      imagePosition.value,
+      [0, 1],
+      [-height / 1, 0]
+    );
+    return {
+      transform: [
+        { translateY: withTiming(interpolation, { duration: 1000 }) },
+      ],
+    };
+  });
+  const buttonAnimatedStyle = useAnimatedStyle(() => {
+    const interpolation = interpolate(imagePosition.value, [0, 1], [250, 0]);
+    return {
+      opacity: withTiming(imagePosition.value, { duration: 500 }),
+      transform: [
+        { translateY: withTiming(interpolation, { duration: 1000 }) },
+      ],
+    };
+  });
+  const closeAnimatedStyle = useAnimatedStyle(() => {
+    const interpolation = interpolate(imagePosition.value, [0, 1], [180, 360]);
+    return {
+      opacity: withTiming(imagePosition.value === 1 ? 0 : 1, { duration: 800 }),
+      transform: [
+        { rotate: withTiming(interpolation + "deg", { duration: 1000 }) },
+      ],
+    };
+  });
+
+  const formButtonAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: formButtonScale.value }],
+    };
+  });
+
+  const loginHandler = () => {
+    imagePosition.value = 0;
+    if (isRegistering) {
+      setIsRegistering(false);
+      runOnJS(setIsRegistering(false));
+    }
+  };
+  const signupHandler = () => {
+    imagePosition.value = 0;
+    if (isRegistering) {
+      setIsRegistering(true);
+      runOnJS(setIsRegistering(true));
+    }
+  };
+  const formAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity:
+        imagePosition.value === 0
+          ? withDelay(400, withTiming(1, { duration: 800 }))
+          : withTiming(0, { duration: 300 }),
+    };
+  });
   return (
-    <View>
-      <Text>login</Text>
-    </View>
+    <Animated.View style={styles.container}>
+      <Animated.View style={(StyleSheet.absoluteFill, imageAnimatedStyle)}>
+        <View style={StyleSheet.absoluteFill} height={height} width={width}>
+          <ClipPath id="clipPathId">
+            <Ellipse cx={width / 2} rx={height} ry={height + 100} />
+          </ClipPath>
+          <ImageBackground
+            source={require("../../assets/login-background.jpg")}
+            resizeMode="cover"
+            width={width + 100}
+            height={height + 100}
+            preserveAspectRatio="xMidYMid slice"
+            clipPath="url(#clipPathId)"
+            style={{ flex: 1, justifyContent: "center", height: "100%" }}
+          />
+
+          <Animated.View
+            style={[styles.closeButtomContainer, closeAnimatedStyle]}
+          >
+            <Text onPress={() => (imagePosition.value = 1)}>X</Text>
+          </Animated.View>
+        </View>
+      </Animated.View>
+
+      <View style={styles.bottomContainer}>
+        {/* fs */}
+        <Animated.View style={buttonAnimatedStyle}>
+          <Pressable style={styles.button} onPress={loginHandler}>
+            <Text style={styles.buttonText}> Login</Text>
+          </Pressable>
+        </Animated.View>
+        <Animated.View style={buttonAnimatedStyle}>
+          <Pressable style={styles.button} onPress={signupHandler}>
+            <Text style={styles.buttonText}>Register</Text>
+          </Pressable>
+        </Animated.View>
+        <Animated.View style={[styles.formInputContainer, formAnimatedStyle]}>
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="black"
+            style={styles.Textinput}
+          />
+
+          <TextInput
+            placeholder="User Name"
+            placeholderTextColor="black"
+            style={styles.Textinput}
+          />
+
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="black"
+            style={styles.Textinput}
+          />
+          <Animated.View style={[styles.formButton, formButtonAnimatedStyle]}>
+            <Pressable
+              onPress={() =>
+                (formButtonScale.value = withSequence(
+                  withSpring(1.1),
+                  withSpring(1)
+                ))
+              }
+            >
+              <Text style={styles.buttonText}>
+                {isRegistering ? "Register" : "Register"}
+              </Text>
+            </Pressable>
+          </Animated.View>
+        </Animated.View>
+      </View>
+    </Animated.View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  button: {
+    backgroundColor: "#857BE1",
+    height: 55,
+    justifyContent: "center",
+    borderRadius: 35,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "white",
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 17,
+    fontWeight: "700",
+    color: "white",
+    letterSpacing: 0.5,
+  },
+  bottomContainer: {
+    justifyContent: "center",
+    height: height / 2,
+  },
+  Textinput: {
+    height: 50,
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 25,
+    paddingLeft: 10,
+  },
+  formButton: {
+    backgroundColor: "#857BE1",
+    height: 55,
+    justifyContent: "center",
+    borderRadius: 35,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  formInputContainer: {
+    marginBottom: 70,
+    ...StyleSheet.absoluteFill,
+    zIndex: -1,
+    justifyContent: "center",
+  },
+  closeButtomContainer: {
+    height: 40,
+    width: 40,
+    justifyContent: "center",
+    left: 175,
+    alignContent: "center",
+    shadowColor: "#000",
 
-const styles = StyleSheet.create({});
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    borderRadius: 20,
+    top: -20,
+  },
+});
